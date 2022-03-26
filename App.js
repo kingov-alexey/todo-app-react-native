5b895d7844935db452312c9b061d8fcf0c096cda
kingov.alexey <kingov_alexey@mail.ru>
2022-03-26 15:32:01 +0600

    2.7 add AddTodo

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Navbar } from './src/Navbar';
+import { AddTodo } from './src/AddTodo';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Navbar title="Todo App" />
+      <View style={styles.container}>
+        <AddTodo />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+  },
 });
