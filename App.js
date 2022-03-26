72eb60d7052d0f0462e806ce036425fbf2f88b85
kingov.alexey <kingov_alexey@mail.ru>
2022-03-26 14:27:37 +0600

    2.4 preparation

--- App.js
+++ App.js
@@ -4,17 +4,25 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      {/* <Text style={{ color: '#fff' }}>Hello world!</Text> инлайн стили*/}
+      <Text style={styles.text}>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 50,
   },
 });
