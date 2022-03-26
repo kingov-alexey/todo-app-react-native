a40350d445dd7bd1ae1210c31e8d5dfa898906fa
kingov.alexey <kingov_alexey@mail.ru>
2022-03-26 15:01:24 +0600

    2.6 how work with doc react native

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
 import { Navbar } from './src/Navbar';
 
 export default function App() {
@@ -11,8 +10,6 @@ export default function App() {
     </View>
   );
 }
-
-//
 
 const styles = StyleSheet.create({
   container: {},
