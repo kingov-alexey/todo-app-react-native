3eb45da2fbf6eb884c7a33ac5f62337cfc91931b
kingov.alexey <kingov_alexey@mail.ru>
2022-03-26 17:40:10 +0600

    2.13 add scroll

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { Navbar } from './src/Navbar';
 import { AddTodo } from './src/AddTodo';
 import { Todo } from './src/Todo';
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'test1' },
+    { id: 2, title: 'test2' },
+    { id: 3, title: 'test3' },
+    { id: 4, title: 'test4' },
+    { id: 5, title: 'test5' },
+    { id: 6, title: 'test6' },
+    { id: 7, title: 'test7' },
+    { id: 8, title: 'test8' },
+    { id: 9, title: 'test9' },
+  ]);
 
   const addTodo = title => {
     // const newTodo = {
@@ -28,7 +38,7 @@ export default function App() {
   };
 
   return (
-    <View>
+    <ScrollView>
       <Navbar title="Todo App" />
       <View style={styles.container}>
         <AddTodo onSubmit={addTodo} />
@@ -40,7 +50,7 @@ export default function App() {
       </View>
 
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
